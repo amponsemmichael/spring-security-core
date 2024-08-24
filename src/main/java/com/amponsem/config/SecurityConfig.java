ea7669a52a596aed6828dbcc9bf701382bfeeac6
amponsemmichael <amponsem_michael@yahoo.com>
2024-08-24 09:57:45 +0000

    add an authorized page for an authenticated user to view

--- src/main/java/com/amponsem/config/SecurityConfig.java
+++ src/main/java/com/amponsem/config/SecurityConfig.java
@@ -29,7 +29,6 @@ public class SecurityConfig {
                 )
                          .httpBasic(withDefaults())
                          .formLogin(withDefaults())
-       //
                          .build();
     }
 
--- src/main/java/com/amponsem/controller/IndexController.java
+++ src/main/java/com/amponsem/controller/IndexController.java
@@ -1,13 +1,10 @@
 package com.amponsem.controller;
 
-
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-
-public class UserController {
-
+@Controller
+public class IndexController {
     @GetMapping("/")
     public String index() {
         return "index";
